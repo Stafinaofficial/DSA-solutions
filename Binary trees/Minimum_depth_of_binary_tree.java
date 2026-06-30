c0f4dda26ac6f1c07572c72d74e4551900fdd49c
Stafina S <stafinaofficial@gmail.com>
2026-06-30 20:33:41 +0530

    Implement minDepth function for binary tree

---
+++ Binary trees/Minimum_depth_of_binary_tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        int count=1;
+        while(!q.isEmpty()){
+            int n=q.size();
+            
+            for(int i=0;i<n;i++){
+                TreeNode node=q.poll();
+                if(node.left==null && node.right==null){
+                    return count;
+                }
+                if(node.left!=null){
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    q.offer(node.right);
+                }
+                
+            }
+            count++;
+        }
+        return -1;
+    }
+}
+
+//Time Complexity:O(N)
+//Space Complexity:O(N)
